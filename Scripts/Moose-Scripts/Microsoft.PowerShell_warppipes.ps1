0d3396018b998a513a9da3ac02fb62468af06d80
Brad Mason <DeadlyBrad42@gmail.com>
2022-09-25 23:03:59 -0400

    Re-adds warppipes in new location

---
+++ Scripts/Moose-Scripts/Microsoft.PowerShell_warppipes.ps1
@@ -0,0 +1,14 @@
+################################################################################
+## Warp Pipes
+################################################################################
+
+
+# I use this file to set up shortcuts to directories, network shares, and boxes
+# for SSH. I call these 'warp pipes' and I use them to make jumping around
+# directories faster
+
+# Jump2 (Shortcuts)
+#function Jump2-Dev                 { cd "C:\dev" }
+
+# SSH2 Shortcuts
+#function SSH2-box1                 { ssh username@example.com }
